afbabee9d1146469d0348709bf21bc1e8741eed6
mferi <mferiaa@gmail.com>
2017-01-09 23:19:16 +0000

    Edit hostname

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't$6p8^=z+%8_zm+qb%s8&!!sh@j%)lg4byd@nc8(s5#ozoz&-l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'mferi.pythonanywhere.com']
 
 
 # Application definition
